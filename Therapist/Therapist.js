284ed03e55693e56b539db9a3a06407fb13bcb06
Jaqueline de Almeida <jaquelinedealmeida@MacBook-Pro-de-Jaqueline.local>
2022-12-05 23:58:28 -0300

    Included test to datas validation

--- Therapist/Therapist.js
+++ Therapist/Therapist.js
@@ -5,15 +5,21 @@ class Therapist {
     #telefone;
     city;
     state;
-    constructor(name, cpf, email, telefone, city, state){
+    registerTherapist(name, cpf, email, telefone, city, state){
         this.name = name;
         this.#cpf = cpf;
         this.#email = email;
         this.#telefone = telefone;
         this.city = city;
         this.state = state
-    }
 
+        return "Therapist was registered with successful.";
+        // throw new ("Register not is correct.") ;
+    }
 }
+
+
+therapist = new Therapist()
+therapist.registerTherapist("Maria", "04567898710", "maria@email.com", "719898989", "Salvador")
 
 module.exports = Therapist;
--- Therapist/Therapist.test.js
+++ Therapist/Therapist.test.js
@@ -1,9 +1,23 @@
 const Therapist = require("./Therapist.js")
 
 describe("Test of the class Therapist", () => {
+    // beforeEach(() => {
+    //     return therapist = new Therapist("Maria", "04567898710", "maria@email.com", "719898989", "Salvador", "Bahia")
+    // });
+
     test("check if the instance Therapist is correct.", () => {
         const therapist = new Therapist();
         expect(therapist instanceof Therapist).toBe(true);
+    });
+
+    test("register therapist with validate dates", () => {
+        const therapist = new Therapist();
+        expect(therapist.registerTherapist("Maria", "04567898710", "maria@email.com", "719898989", "Salvador", "Bahia")).toBe("Therapist was registered with successful.")
+    });
+
+    test("Register therapist with invalidate dates", () => {
+        const therapist = new Therapist();
+        expect(therapist.registerTherapist("Maria", "04567898710", "maria@email.com", "719898989", "Salvador")).toThrow("Register not is correct.")
     });
 
     
