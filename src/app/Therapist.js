de267207fad9c692f83af78b7f067d54042c0964
Jaqueline de Almeida <jaquelinedealmeida@MacBook-Pro-de-Jaqueline.local>
2022-12-09 17:09:33 -0300

    Constructor of the class modified and include validations methods.

---
+++ src/app/Therapist.js
@@ -0,0 +1,82 @@
+class Therapist {
+  
+  fullName = "";
+  #cpf = "";
+  email = "";
+  telephone = "";
+  city = "";
+  state = "";
+  curriculum = false;
+  id;
+
+    constructor(fullName, cpf, email, telephone, city, state, curriculum){
+      
+      this.fullName = fullName;
+      this.validateCpf(cpf)
+      this.validateEmail(email);
+      this.validateTelephone (telephone);
+      this.city = city;
+      this.state = state;
+      this.validateCurriculum(curriculum);
+      this.id = id;
+    }
+
+    validateCpf(cpf){
+      let regexCPF = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
+
+      if(!regexCPF.test(cpf)){
+        throw new Error ("CPF number is not valid.");
+      }
+        this.#cpf = cpf;
+        console.log("CPF is validate.")
+    }
+
+    get valueCpf(){
+      return this.#cpf;
+    }
+
+    validateEmail(email){
+      let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+      if(!regexEmail.test(email)){
+        throw new Error ("Address e-mail is not valid.");
+      }
+        this.email = email;
+        return "E-mail address is correct."
+    }
+
+    validateTelephone(telephone){
+      let regexTelephone = /^\+?\(?([0-9]{2})?\)?\s?[0-9]{4,5}(-|\s)?[0-9]{4}$/;
+
+      if(!regexTelephone.test(telephone)){
+        throw new Error ("Telephone number is not valid.");
+      }
+        this.telephone = telephone;
+        return "Telephone number is correct."
+    }
+
+    validateCurriculum(curriculum){
+      if(!curriculum){
+        throw new Error ("Curriculum not is validate");
+      }
+        this.curriculum = curriculum
+        return "Curriculum is validate."
+    }
+
+    static Therapists ={
+        therapistAba: [],
+        speechTherapy: [],
+        occupationalTherapist: []
+    }
+
+}
+
+const id = Math.floor(Date.now() * Math.random()).toString(30);
+
+const therapist = new Therapist("Maria Santana", "04567898710", "maria@email.com", "71 989898989", "Salvador", "Bahia", true);
+console.log(therapist);
+
+console.log(therapist.valueCpf);
+
+
+export default Therapist;
